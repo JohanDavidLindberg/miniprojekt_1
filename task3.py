db75a7422b4c8298589d15034c2bbe244859f2d8
Johan Lindberg <johanlindberg@outlook.com>
2026-04-09 12:39:52 +0200

    Del 3 kalr

--- task3.py
+++ task3.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 
-x = np.zeros((2000, 2), dtype=float)
+x = np.x = np.empty((0, 2), dtype=float)
 u = np.array([0.3,0])
 h = 0.1
 D = 0.02
@@ -14,9 +14,10 @@ c = []
 states = []
 ccalc = 0
 while T <=60:
+    for i in range(10):
+        x = np.vstack([x, [0,0]])
     for i in range(len(x)):
         x[i] += u*h + np.sqrt(2*D*h)*np.random.multivariate_normal([0, 0], np.eye(2))
-    
     if T % 15 == 0 and (T != 0):
         c=[]
         ccalc = 0
@@ -40,7 +41,6 @@ for i in range(len(Cstaes)):
     axs.flat[i].set_ylim(-5, 5)
     axs.flat[i].set_title(f'Tid: {15*(i+1)} s')
 plt.show()
-
 
 fig, axs = plt.subplots(2, 2, figsize=(10, 8))
 for i in range(len(states)):
